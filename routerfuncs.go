3dd923955ae302a9ad496c82aee38f2a8db8e9e1
dilungasr <dilungasalumtz@gmail.com>
2021-10-17 15:08:50 +0300

    Added router methods or actions

---
+++ routerfuncs.go
@@ -0,0 +1,56 @@
+package dnet
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/dilungasr/radi"
+	tz "github.com/dilungasr/tanzanite"
+)
+
+// On method is adding the Event handlers to the router
+func On(action string, handlers ...ActionHandler) {
+	Router1.actionHandlers[action] = handlers
+}
+
+// Router is for grouping the actions by matching their paths
+func Router(path string) RouterMatcher {
+	return RouterMatcher{path}
+}
+
+// Use is for adding middlewares to the root of the dnet action path
+func Use(handlers ...ActionHandler) {
+	Router1.routeMatchers["/"] = append(Router1.routeMatchers["/"], handlers...)
+}
+
+// SendTicket sends an encrypted ticket to the use and saves the clean one the router
+func SendTicket(r *http.Request, w http.ResponseWriter, ID string) {
+	// if the ticketSecrete and the iv set...... wer are ready to go
+	secreteKey := Router1.ticketSecrete
+	iv := Router1.ticketIV
+
+	// plain data(not encrypted)
+	expireTimeBytes, err := time.Now().Local().Add(Router1.ticketAge).MarshalText()
+	if err != nil {
+		panic(err)
+	}
+	expireTime := string(expireTimeBytes)
+	// get the user IP
+	IP, err := tz.GetIP(r)
+	if err != nil {
+		tz.Send(w, 400, tz.Map{"msg": "Bad request!"})
+		return
+	}
+
+	// encrypt the tikcet data to be sent to the client
+	newTicket := ID + "," + IP + "," + expireTime
+
+	encTicket := radi.Encrypt(newTicket, secreteKey, iv)
+
+	// save the ticket in the Router1
+	Router1.tickets = append(Router1.tickets, newTicket)
+
+	// send the ticket to the client
+	tz.Send(w, tz.Map{"ticket": encTicket})
+
+}
